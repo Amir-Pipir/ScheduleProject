89668bcea51eecfcca5d7b23a2ded86a5b0d8f96
Amir-Pipir <amirlu2006@gmail.com>
2026-05-29 18:28:37 +0300

    s5

--- schedule_project/settings.py
+++ schedule_project/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
+    "PAGE_SIZE": 400,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
